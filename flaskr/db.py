cab01a333618d79462be13f88bce3f72f35b290e
shaxl <actionplan2456@protonmail.com>
2022-04-02 20:29:48 -0500

    fixed init-db

--- flaskr/db.py
+++ flaskr/db.py
@@ -35,6 +35,6 @@ def init_db_command():
     init_db()
     click.echo('Initialized the database.')
 
-def init_app(appplication):
+def init_app(app):
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
